113af95674c4ce4b694eb87d3312b7e53daf1e93
seanianking <seanianking@gmail.com>
2020-05-23 10:22:26 -0600

    added CRUD routes

--- CSDSpEdForm/server.js
+++ CSDSpEdForm/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 
 const db = require('./app/models');
+
 db.sequelize.sync({ force: true }).then(() => {
     console.log("Drop and re-sync db.");
 });
